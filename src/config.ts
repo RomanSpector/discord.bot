aa9fae09a8bf5ec54022d3e18b2b38c155cee537
Roman Spector <ismailroma@ya.ru>
2023-01-05 20:16:31 +0300

    add config

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,17 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = process.env;
+const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN, LOG_CHNNEL_ID } = process.env;
 
-if (!CLIENT_ID || !GUILD_ID || !DISCORD_TOKEN) {
+if (!CLIENT_ID || !GUILD_ID || !DISCORD_TOKEN || !LOG_CHNNEL_ID) {
     throw new Error("Missing environment variables")
 }
 
 const config: Record<string, string> = {
     CLIENT_ID,
     GUILD_ID,
-    DISCORD_TOKEN
+    DISCORD_TOKEN,
+    LOG_CHNNEL_ID
 }
 
 export default config
