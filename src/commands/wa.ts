c38742e0b9a03b52dc1d1d8acedf3a5de12019b2
Roman Spector <ismailroma@ya.ru>
2023-01-06 18:19:36 +0300

    ref

--- src/commands/wa.ts
+++ src/commands/wa.ts
@@ -5,18 +5,20 @@ export const data = new SlashCommandBuilder()
     .setName("wa")
     .setDescription("Link WeakAuras last version")
 
-const button = new ButtonBuilder()
-    .setLabel("Download")
-    .setURL("https://github.com/Bunny67/WeakAuras-WotLK/archive/refs/heads/master.zip")
-    .setStyle(ButtonStyle.Link)
-    .setEmoji({ name: `🔃` })
+const button = new ButtonBuilder({
+    label: "Download",
+    style: ButtonStyle.Link,
+    url: "https://github.com/Bunny67/WeakAuras-WotLK/archive/refs/heads/master.zip",
+    emoji: { name: `🔃` },
+})
 
-const embeds = new EmbedBuilder()
-    .setTitle("WeakAuras")
-    .setColor(0x13ffff)
-    .setImage("https://i.imgur.com/wwbxeCG.jpeg")
-    .setURL("https://github.com/Bunny67/WeakAuras-WotLK")
-    .setDescription("WeakAuras is a powerful and flexible framework that allows the display of highly customizable graphics on World of Warcraft's user interface to indicate buffs, debuffs, and other relevant information. This addon was created to be a lightweight replacement for Power Auras but has since introduced more functionalities while remaining efficient and easy to use.")
+const embeds = new EmbedBuilder({
+    title: "WeakAuras",
+    color: 0x13ffff,
+    image: { url: "https://i.imgur.com/wwbxeCG.jpeg" },
+    url: "https://github.com/Bunny67/WeakAuras-WotLK",
+    description: "WeakAuras is a powerful and flexible framework that allows the display of highly customizable graphics on World of Warcraft's user interface to indicate buffs, debuffs, and other relevant information. This addon was created to be a lightweight replacement for Power Auras but has since introduced more functionalities while remaining efficient and easy to use."
+})
 
 const actionRow = new ActionRowBuilder<ButtonBuilder>().addComponents(button)
 
